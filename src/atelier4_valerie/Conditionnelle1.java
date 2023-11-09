ab5eef0c541f9d1fbdea30403e0d621b8df54e90
isha <isha@fedora>
2023-11-09 10:41:00 +0100

    atelier4

---
+++ src/atelier4_valerie/Conditionnelle1.java
@@ -0,0 +1,16 @@
+package atelier4_valerie;
+
+public class Conditionnelle1 {
+
+	public static void main(String[] args) {
+		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+		
+		if (p1>p2) {
+			if (p1>p3);
+		}
+		int p6;
+		
+		System.out.println(p4);
+	}
+
+}
