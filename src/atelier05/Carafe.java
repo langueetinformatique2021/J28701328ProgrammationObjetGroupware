1de2eef68caebdb5e5fb1ce66cd3a342e88749bd
isha <isha@fedora>
2023-11-09 12:04:22 +0100

    atelier05

---
+++ src/atelier05/Carafe.java
@@ -0,0 +1,45 @@
+package atelier05;
+
+/**
+ * On propose de considérer la carafe comme un objet ayant deux attributs privés
+ * (contenu_ et capacité_) et l’interface publique suivante reprenant les
+ * opérations ci dessus (Remplir, Vider, Contenu, Capacité et Transvaser)
+ */
+
+public class Carafe {
+	private int contenu_, capacité_;
+	
+	public Carafe(int capacité) {
+		capacité_ = capacité;
+		contenu_ = 0;
+	}
+
+	public void Remplir() {
+		contenu_ = capacité_;
+
+	}
+
+	public void Vider() {
+		contenu_ = 0;
+
+	}
+
+	public int Contenu() {
+		return contenu_;
+
+	}
+
+	public int Capacité() {
+		return capacité_;
+
+	}
+
+	public void Transvaser(Carafe c2) {
+		if ((c2.Capacité() - c2.Contenu()) == this.Contenu()) {
+			this.Vider();
+			c2.Remplir();
+		}
+
+	}
+
+}
---
+++ src/atelier05/testCarafe.java
@@ -0,0 +1,29 @@
+package atelier05;
+
+public class testCarafe {
+
+	public static void main(String[] args) {
+		Carafe c1, c2;
+		
+		c1 = new Carafe(5);
+		System.out.println(c1.Capacité());
+		System.out.println(c1.Contenu());
+		
+		c2 = new Carafe(7);
+		System.out.println(c2.Capacité());
+		System.out.println(c2.Contenu());
+		
+		c1.Remplir();
+		System.out.println(c1.Contenu());
+		
+		c2.Remplir();
+		System.out.println(c2.Contenu());
+		c2.Vider();
+		System.out.println(c2.Contenu());
+		
+		c1.Transvaser(c2);
+
+
+	}
+
+}
